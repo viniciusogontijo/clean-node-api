79b0b8bd08d118c0cce8252b14e24f5b5227edaf
vinicius gontijo <vinicius.gontijo@rarolabs.com.br>
2022-05-28 14:11:42 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
+  coverageDirectory: 'coverage',
+  roots: ['<rootDir/src>'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
